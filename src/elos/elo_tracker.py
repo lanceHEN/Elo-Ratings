5b0fc1aca57f7ee43295d5152bce2e05de59cd1b
lancehendricks <hendricks.la@northeastern.edu>
2025-10-12 17:02:58 -0400

    Added work on win probability estimates

--- src/elos/elo_tracker.py
+++ src/elos/elo_tracker.py
@@ -9,11 +9,14 @@ class EloTracker(object):
     Attributes:
         elos_map (Dict[str, List[Tuple[pd.Timestamp, float, int, int, int]]]): Mapping from each team to a 
             chronologically ordered list of tuples containing:
-            (1) the date/time their Elo updated,
-            (2) their Elo after that update occurred,
-            (3) their number of wins after that update occurred,
-            (4) their number of losses after that update occurred,
-            (5) the current season.
+            (1) the game id,
+            (2) the date/time their Elo updated,
+            (3) their Elo before that update occurred,
+            (4) their Elo after that update occurred,
+            (5) 1 if they won or 0 if they lost,
+            (6) their number of wins after that update occurred,
+            (7) their number of losses after that update occurred,
+            (8) the current season.
             This is the centerpoint of this class and may be referenced at any time
             to observe a team's Elo history.
         initial_elo (float): The initial Elo rating for each team. This will be used for the
@@ -71,38 +74,44 @@ class EloTracker(object):
         away_elo = away_elo + int(K*(away_won - away_win_prob))
     
         return home_elo, away_elo
-    
-    def _initialize_history_if_necessary(self, team: str, dt: pd.Timestamp, season: int) -> None:
-        """If elos_map[team] == [], then an initial entry will created in elos_map
-        for the given team for 00:00:00 the day before the given timestamp,
-        with their Elo being initial_elo, and having 0 wins and 0 losses for the given season.
-        
-        Args:
-            team (str): The team to check.
-            dt (pd.Timestamp): The game timestamp.
-            season (int): The season the game takes place in.
-        """
-        if self.elos_map[team] == []:
-            t = (get_prev_date_midnight(dt), self.initial_elo, 0, 0, season)
-            self.elos_map[team].append(t)
             
-    def _initialize_new_season_entry_if_necessary(self, team: str, dt: pd.Timestamp, season: int) -> None:
-        """If the last recorded season in elos_map[team] < season, this will add another
-        entry in elos_map for the given team for 00:00:00 the day before the given timestamp,
-        with their Elo being their previous elo reverted to initial_elo by 1/3, and having
-        0 wins and 0 losses.
+    def _get_initial_elo(self, team: str, season: int) -> float:
+        """Fetches an initial Elo for the team. If elos_map[team] is empty, it will produce initial_elo.
+        If the season is a new season, it will be the team's previous elo reverted to initial_elo by 1/3.
+        Otherwise, it will just be the previous Elo.
         
         Args:
             team (str): The team to check.
-            dt (pd.Timestamp): The game timestamp.
             season (int): The possibly new season to check.
         """
-        if self.elos_map[team][-1][4] < season:
-            old_elo = self.elos_map[team][-1][1]
+        
+        if self.elos_map[team] == []:
+            return self.initial_elo
+
+        elif self.elos_map[team][-1][7] < season:
+            old_elo = self.elos_map[team][-1][3]
             new_elo = old_elo + int((self.initial_elo - old_elo) / 3)
-            
-            t = (get_prev_date_midnight(dt), new_elo, 0, 0, season)
-            self.elos_map[team].append(t)
+            return new_elo
+        
+        else:
+            return self.elos_map[team][-1][3]
+        
+    def _get_team_record(self, team: str, season: int) -> Tuple[int, int]:
+        """Fetches the wins and losses for the team. If elos_map[team] is empty, or the season
+        is a new season, it will produce 0, 0. Otherwise, it will just be the previous wins and losses.
+        
+        Args:
+            team (str): The team to check.
+            season (int): The possibly new season to check.
+        """
+        
+        if self.elos_map[team] == [] or self.elos_map[team][-1][7] < season:
+            return 0, 0
+        
+        else:
+            wins = self.elos_map[team][-1][5]
+            losses = self.elos_map[team][-1][6]
+            return wins, losses
     
     def add_history(self, game_df: pd.DataFrame) -> None:
         """Adds the result and updated Elo for every game in game_df to self.elos_map.
@@ -121,9 +130,10 @@ class EloTracker(object):
                 including columns 'hometeam' for the home team, 'visteam' for the away team, and
                 'homewon' which is 1 if home won and 0 otherwise. Each game in game_df must take
                 place after the games that have already been logged for the given teams it includes.
+                Must be indexed by a game id column 'gid'.
         """
         
-        for _, game in game_df.iterrows():
+        for game_id, game in game_df.iterrows():
             home_team = game['hometeam']
             away_team = game['visteam']
             
@@ -132,40 +142,31 @@ class EloTracker(object):
             
             season = game['season']
             
-            # Adds initial entry to elos_map for the team if it is empty
-            self._initialize_history_if_necessary(home_team, timestamp, season)
-            self._initialize_history_if_necessary(away_team, timestamp, season)
+            # Get initial elos
+            initial_home_elo = self._get_initial_elo(home_team, season)
+            initial_away_elo = self._get_initial_elo(away_team, season)
             
-            # Checks if current season > last recorded season, if so reverting to initial_elo by 1/3.
-            self._initialize_new_season_entry_if_necessary(home_team, timestamp, season)
-            self._initialize_new_season_entry_if_necessary(away_team, timestamp, season)
+            # Get initial home/away wins and losses
+            home_wins, home_losses = self._get_team_record(home_team, season)
+            away_wins, away_losses = self._get_team_record(away_team, season)
             
             # Final result
             home_won = game['homewon']
             away_won = 1 - home_won
             
-            home_elo = self.elos_map[home_team][-1][1]
-            away_elo = self.elos_map[away_team][-1][1]
-            
-            home_elo, away_elo = EloTracker._elo_update(home_elo, away_elo, home_won, self.K)
+            updated_home_elo, updated_away_elo = EloTracker._elo_update(initial_home_elo, initial_away_elo, home_won, self.K)
             
             # Update records
-        
-            home_wins = self.elos_map[home_team][-1][2]
-            home_losses = self.elos_map[home_team][-1][3]
-        
+    
             home_wins += home_won
             home_losses += away_won
             
-            away_wins = self.elos_map[away_team][-1][2]
-            away_losses = self.elos_map[away_team][-1][3]
-        
             away_wins += away_won
             away_losses += home_won
         
             # Add to elos_map
-            h_tuple = (timestamp, home_elo, home_wins, home_losses, season)
-            a_tuple = (timestamp, away_elo, away_wins, away_losses, season)
+            h_tuple = (game_id, timestamp, initial_home_elo, updated_home_elo, home_won, home_wins, home_losses, season)
+            a_tuple = (game_id, timestamp, initial_away_elo, updated_away_elo, away_won, away_wins, away_losses, season)
             self.elos_map[home_team].append(h_tuple)
             self.elos_map[away_team].append(a_tuple)
         
--- src/utils/utils.py
+++ src/utils/utils.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 import seaborn as sns
 from matplotlib import pyplot as plt
 from typing import Set, Dict, List, Tuple
@@ -12,11 +13,13 @@ def get_prev_date_midnight(dt: pd.Timestamp) -> pd.Timestamp:
 
 def load_all_games_csv(filename: str) -> pd.DataFrame:
     """Prodcuces filename as a Dataframe, doing any
-    necessary operations on it such as getting the correct dtypes."""
+    necessary operations on it such as getting the correct dtypes and setting
+    the index to the game id."""
     all_games = pd.read_csv(filename)
     
     # Initially string, must be made timestamp
     all_games['timestamp'] = pd.to_datetime(all_games['timestamp'])
+    all_games = all_games.set_index('gid')
     return all_games
 
 def get_teams(game_df: pd.DataFrame) -> Set[str]:
@@ -31,20 +34,51 @@ def plot_elo_ratings_over_time(team: str, elos_map: Dict[str, List[Tuple[pd.Time
         team (str): The team whose Elo ratings will be plotted.
         elos_map (Dict[str, List[Tuple[pd.Timestamp, float, int, int]]]): Mapping from each team to a 
             non-empty, chronologically ordered list of tuples containing:
-            (1) the date/time their Elo updated,
-            (2) their Elo after that update occurred,
-            (3) their number of wins after that update occurred,
-            (4) their number of losses after that update occurred.
+            (1) the game id,
+            (2) the date/time their Elo updated,
+            (3) their Elo before that update occurred,
+            (4) their Elo after that update occurred,
+            (5) 1 if they won or 0 if they lost,
+            (6) their number of wins after that update occurred,
+            (7) their number of losses after that update occurred,
+            (8) the current season.
     """
-    dates = [elos_map[team][i][0] for i in range(len(elos_map[team]))]
-    elos = [elos_map[team][i][1] for i in range(len(elos_map[team]))]
+    dates = [get_prev_date_midnight(elos_map[team][0][1])] + [elos_map[team][i][1] for i in range(len(elos_map[team]))]
+    elos = [elos_map[team][i][2] for i in range(len(elos_map[team]))] + [elos_map[team][-1][3]]
     
     data = {'Date':dates, 'Elos':elos}
     
-    sns.lineplot(data=data, x='Date', y='Elos')
     plt.grid()
+    sns.lineplot(data=data, x='Date', y='Elos')
     plt.xticks(rotation=45, ha='right')
     plt.xlabel('Date')
     plt.ylabel('Elo')
     plt.title(f'Elo Over Time for {team}')
     plt.show()
+    
+def plot_elos_distribution(teams: Set[str], elos_map: Dict[str, List[Tuple[pd.Timestamp, float, int, int]]]) -> Tuple[float, float]:
+    """Plots the distribution of the latest elos for each team in elos_map, returning the mean and std.
+    
+    Args:
+        elos_map (Dict[str, List[Tuple[pd.Timestamp, float, int, int]]]): Mapping from each team to a 
+            non-empty, chronologically ordered list of tuples containing:
+            (1) the game id,
+            (2) the date/time their Elo updated,
+            (3) their Elo before that update occurred,
+            (4) their Elo after that update occurred,
+            (5) 1 if they won or 0 if they lost,
+            (6) their number of wins after that update occurred,
+            (7) their number of losses after that update occurred,
+            (8) the current season.
+    """
+
+    latest_elos = np.array([elos_map[team][-1][3] for team in teams])
+    
+    plt.grid()
+    plt.hist(latest_elos)
+    plt.xlabel('Elo Rating')
+    plt.ylabel('Count')
+    plt.title('Elo Ratings Counts')
+    plt.show()
+    
+    return  np.mean(latest_elos), np.std(latest_elos)
